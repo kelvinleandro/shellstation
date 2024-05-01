e2da03bd58a47adb654edfecd6f0b48c1b33485b
kelvinleandro <kelvinleandro5@gmail.com>
2024-05-01 20:27:41 -0300

    add command-line game selection feature

--- shellstation.py
+++ shellstation.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 import inquirer
+import argparse
 from hangman.hangman import Hangman
 from tic_tac_toe.tictactoe import TicTacToe
 
 
 @dataclass
 class ShellStation:
-  games = [Hangman, TicTacToe]
-  names = [Game.__name__ for Game in games]
+  games = {'Hangman': Hangman, 'TicTacToe': TicTacToe}
 
 
   def display_logo(self):
@@ -16,13 +16,30 @@ class ShellStation:
     print(logo)
   
 
-  def start(self):
-    self.display_logo()
-    print('Welcome to ShellStation!')
-    choice = inquirer.list_input("Select the game you wanna play", choices=self.names)
-    index = self.names.index(choice)
-    self.games[index]().start() # initializes the selected game
+  def start(self, game_name=None):
+    if game_name:
+      game_class = self.games.get(game_name)
+      if game_class:
+        game_class().start()
+    else:
+      self.display_logo()
+      print('Welcome to ShellStation!')
+      choice = inquirer.list_input("Select the game you wanna play", choices=self.games.keys())
+      self.games[choice]().start()
+
+
+def parse_arguments():
+  parser = argparse.ArgumentParser(description="Run a specific game directly from the ShellStation game suite.")
+  valid_games = ['Hangman', 'TicTacToe']
+  parser.add_argument('-g', '--game', type=str, choices=valid_games,
+                      help="Specify the game to play.")
+  return parser.parse_args()
 
 
 if __name__ == "__main__":
-  ShellStation().start()
+  args = parse_arguments()
+  shell_station = ShellStation()
+  if args.game:
+    shell_station.start(game_name=args.game)
+  else:
+    shell_station.start()

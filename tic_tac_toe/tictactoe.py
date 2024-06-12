6ebb02a12215e9046560d24108e97853fb6f937d
kelvinleandro <kelvinleandro5@gmail.com>
2024-06-12 17:41:51 -0300

    refactor(tictactoe): align code patterns with chess and draughts for consistency

--- tic_tac_toe/tictactoe.py
+++ tic_tac_toe/tictactoe.py
@@ -1,9 +1,18 @@
 import os
+import socket
+import threading
 from .board import Board
 
 class TicTacToe:
   def __init__(self):
-    self.game = Board()
+    self.board = Board()
+    self.turn = "X"
+    self.you = None
+    self.opponent = None
+    self.winner = None
+    self.game_over = False
+    self.counter = 0
+
 
   def display_logo(self) -> None:
     logo_path = os.path.join(os.path.dirname(__file__), "logo.txt")
@@ -14,10 +23,98 @@ class TicTacToe:
     except FileNotFoundError:
       pass
 
+
   def start(self) -> None:
     self.display_logo()
     host, port = input('Specify the host and port:\n').split()
-    self.game.connect_to_game(host, int(port))
+    self.connect_to_game(host, int(port))
+
+
+  def connect_to_game(self, host: str, port: int) -> None:
+    is_host = False
+    try:
+      server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      server.bind((host, port))
+      server.listen(1)
+
+      client, addr = server.accept()
+      is_host = True
+    except OSError as e:
+      if e.errno == 10048:  # Address already in use, attempt to connect as client
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, port))
+      else:
+        raise e
+
+    if is_host:
+      self.you = "X"
+      self.opponent = "O"
+      threading.Thread(target=self.handle_connection, args=(client,)).start()
+      server.close()
+    else:
+      self.you = "O"
+      self.opponent = "X"
+      threading.Thread(target=self.handle_connection, args=(client,)).start()
+
+
+  def handle_connection(self, client: socket.socket) -> None:
+    while not self.game_over:
+      os.system('cls' if os.name == 'nt' else 'clear')
+      self.board.print_board()
+      if self.turn == self.you:
+        while True:
+          move = input("Enter a move (e.g., 0,0): ")
+          if self.board.check_valid_move(move.split(',')):
+            client.send(move.encode('utf-8'))
+            self.board.apply_move(move.split(','), self.you)
+            self.turn = self.opponent
+            break
+          else:
+            print('Invalid move! Try again.')
+      else:
+        data = client.recv(1024)
+        if not data:
+          break
+        else:
+          self.board.apply_move(data.decode('utf-8').split(','), self.opponent)
+          self.turn = self.you
+      self.counter += 1
+      self.update_game_over()
+    if self.winner == self.you:
+      print("You win")
+    elif self.winner == self.opponent:
+      print("You lose")
+    elif self.counter == 9:
+      print("It is a tie.")
+
+
+  def update_game_over(self) -> None:
+    board = self.board.board
+    for row in range(3):
+      if board[row][0] == board[row][1] == board[row][2] != " ":
+        self.winner = board[row][0]
+        self.game_over = True
+        return
+
+    for column in range(3):
+      if board[0][column] == board[1][column] == board[2][column] != " ":
+        self.winner = board[0][column]
+        self.game_over = True
+        return
+
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+      self.winner = board[0][0]
+      self.game_over = True
+      return
+      
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+      self.winner = board[0][2]
+      self.game_over = True
+      return
+
+    if self.counter == 9:
+      self.game_over = True
+
 
 if __name__ == "__main__":
   TicTacToe().start()
